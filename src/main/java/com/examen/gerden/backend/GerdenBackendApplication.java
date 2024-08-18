55f8b7d29257a44fa4e17f89e913186cfef37d12
Youva97 <mineas.gael@outlook.com>
2024-08-18 14:40:15 +0200

    premier commit pour l'app du forum et massagerie privée

---
+++ src/main/java/com/examen/gerden/backend/GerdenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.examen.gerden.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerdenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerdenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/gerden/backend/GerdenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.gerden.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerdenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
